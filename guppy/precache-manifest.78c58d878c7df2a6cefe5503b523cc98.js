7d63c59e67d1ffe33263e00eed3ad3aee653ce59
Anton Schreiner <antonschrein@gmail.com>
2019-06-13 06:21:28 +0000

    add an early build of guppy

---
+++ guppy/precache-manifest.78c58d878c7df2a6cefe5503b523cc98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "227000c539e754483a25",
+    "url": "/guppy/static/css/main.57a4813c.chunk.css"
+  },
+  {
+    "revision": "227000c539e754483a25",
+    "url": "/guppy/static/js/main.227000c5.chunk.js"
+  },
+  {
+    "revision": "95d06f4b6d72e40e836b",
+    "url": "/guppy/static/js/runtime~main.95d06f4b.js"
+  },
+  {
+    "revision": "781d5dc98d718c1654d7",
+    "url": "/guppy/static/js/2.781d5dc9.chunk.js"
+  },
+  {
+    "revision": "14a6db4a96d131a92542",
+    "url": "/guppy/static/js/3.14a6db4a.chunk.js"
+  },
+  {
+    "revision": "14a6db4a96d131a92542",
+    "url": "/guppy/fae5f486ffe029ea9379.module.wasm"
+  },
+  {
+    "revision": "1daf0175fc690a8c52521f45fd48bda3",
+    "url": "/guppy/index.html"
+  }
+];
