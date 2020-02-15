0287c930bd3f2f3a5b710647c61bde53bf02ed5d
Anton Schreiner <antonschrein@gmail.com>
2020-02-15 15:10:08 +0000

    Fix the root of thingy

---
+++ thingy/precache-manifest.420da114e0980d7594a345a3e1ceaea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100d707a0cee6bfba3dc89735317d375",
+    "url": "/thingy/index.html"
+  },
+  {
+    "revision": "59f69348238a8528b22f",
+    "url": "/thingy/static/css/2.7ef8b5c5.chunk.css"
+  },
+  {
+    "revision": "f6b6bfbf2926f13be9b3",
+    "url": "/thingy/static/css/main.a51890ff.chunk.css"
+  },
+  {
+    "revision": "59f69348238a8528b22f",
+    "url": "/thingy/static/js/2.efa03940.chunk.js"
+  },
+  {
+    "revision": "3bd7d177bd02a27587d19dc6a7c7bc08",
+    "url": "/thingy/static/js/2.efa03940.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6b6bfbf2926f13be9b3",
+    "url": "/thingy/static/js/main.a43ece3f.chunk.js"
+  },
+  {
+    "revision": "fa8ff4bc39902e894495",
+    "url": "/thingy/static/js/runtime-main.92f03bab.js"
+  }
+]);
--- thingy/service-worker.js
+++ thingy/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d4d63b266fcd12bd510e8ccbaf7d9dd.js"
+  "/thingy/precache-manifest.420da114e0980d7594a345a3e1ceaea7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thingy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
